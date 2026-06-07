994f7900e4f05fac5fc7187da031d41df494cfb8
Norix10 <norix123321@gmail.com>
2026-06-07 22:24:05 +0300

    add init file to model foalder

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .base import Base
+from .booking import Booking
+from .payment import Payment
+from .room import Room
+from .room_type import RoomType
+from .user import User
+
+__all__ = [Base, Booking, Payment, Room, RoomType, User]
