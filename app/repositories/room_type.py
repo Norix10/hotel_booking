43d94e0b7386518af4f5f6c5cfb4669007b9338c
Norix10 <norix123321@gmail.com>
2026-06-09 18:42:01 +0300

    add new func get_by_id in room_type repository (for int dt)

--- app/repositories/room_type.py
+++ app/repositories/room_type.py
@@ -10,3 +10,7 @@ from app.models.room_type import RoomType
 class RoomTypeRepository(BaseRepository[RoomType]):
     def __init__(self, session: AsyncSession):
         super().__init__(RoomType, session)
+
+    async def get_by_id(self, id: int) -> RoomType | None:
+        result = await self.session.execute(select(RoomType).where(RoomType.id == id))
+        return result.scalar_one_or_none()
