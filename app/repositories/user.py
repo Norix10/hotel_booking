0c722c57bcfef4780a13daf71758b1f642666077
Norix10 <norix123321@gmail.com>
2026-06-09 18:40:53 +0300

    update user repository (deleted copy get_by_id func)

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -11,10 +11,6 @@ class UserRepository(BaseRepository[User]):
     def __init__(self, session: AsyncSession):
         super().__init__(User, session)
 
-    async def get_by_id(self, id: UUID) -> User:
-        result = await self.session.execute(select(User).where(User.id == id))
-        return result.scalar_one_or_none()
-
     async def get_by_email(self, email: str) -> User:
         result = await self.session.execute(select(User).where(User.email == email))
         return result.scalar_one_or_none()
