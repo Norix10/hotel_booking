420e03b7c947f73de4b8fd7069c826dcaa3806cb
Norix10 <norix123321@gmail.com>
2026-06-13 22:45:19 +0300

    feat: implement CRUD operations for room types in the API

---
+++ app/routers/v1/room_types.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Depends, Security, status
+
+from app.schemas.room_type import (
+    RoomTypeResposeSchema,
+    RoomTypeCreateSchema,
+    RoomTypeUpdateSchema,
+)
+from app.core.dependencies import (
+    get_room_type_service,
+    get_current_admin,
+)
+from app.models.user import User
+from app.services.room_type import RoomTypesService
+
+router = APIRouter(prefix="/room-types", tags=["room types"])
+
+
+@router.post("/", response_model=RoomTypeResposeSchema)
+async def create_room_type(
+    data: RoomTypeCreateSchema,
+    current_admin: User = Security(get_current_admin),
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+) -> RoomTypeResposeSchema:
+    return room_type_service.create_room_type(data)
+
+
+@router.get("/{room_type_id}", response_model=RoomTypeResposeSchema)
+async def get_room_type_by_id(
+    room_type_id: int,
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+) -> RoomTypeResposeSchema:
+    return room_type_service._get_room_type_or_404(room_type_id)
+
+
+@router.get("/all", response_model=RoomTypeResposeSchema)
+async def get_all_room_types(
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+    skip: int = 0,
+    limit: int = 100,
+):
+    return room_type_service.get_all(skip, limit)
+
+
+@router.patch("/{room_type_id}", response_model=RoomTypeResposeSchema)
+async def update_room_type_by_id(
+    room_type_id: int,
+    data: RoomTypeUpdateSchema,
+    current_admin: User = Security(get_current_admin),
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+) -> RoomTypeResposeSchema:
+    return room_type_service.update_room_type(room_type_id, data)
+
+
+@router.delete("/{room_type_id}")
+async def delete_room_type_by_id(
+    room_type_id: int,
+    current_admin: User = Security(get_current_admin),
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+):
+    return room_type_service.delete_room_type(room_type_id)
+
+
+@router.delete("/all", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_all_room_types(
+    room_type_id: int,
+    current_admin: User = Security(get_current_admin),
+    room_type_service: RoomTypesService = Depends(get_room_type_service),
+):
+    return room_type_service.delete_all_room_types(room_type_id)
