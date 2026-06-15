137e6f4ca6dc32ebbffcb61a14f944d9c37be3b2
Norix10 <norix123321@gmail.com>
2026-06-16 01:36:12 +0300

    feat: add new routers to admin

--- app/routers/v1/admin.py
+++ app/routers/v1/admin.py
@@ -1,5 +1,6 @@
 from uuid import UUID
-from fastapi import APIRouter, Depends, Security, status
+from typing import Optional
+from fastapi import APIRouter, Depends, Security, status, Query
 
 from app.schemas.booking import BookingResponseSchema, BookingAdminUpdateSchema
 from app.schemas.payments import PaymentResponseSchema, PaymentUpdateSchema
@@ -13,6 +14,36 @@ from app.services.booking import BookingService
 from app.services.payments import PaymentsService
 
 router = APIRouter(prefix="/admin", tags=["admin"])
+
+
+@router.get("/bookings/{booking_id}", response_model=BookingResponseSchema)
+async def get_booking_by_id(
+    booking_id: UUID,
+    current_admin: User = Security(get_current_admin),
+    booking_service: BookingService = Depends(get_booking_service),
+) -> BookingResponseSchema:
+    return await booking_service.get_booking_by_id(booking_id)
+
+
+# @router.get("/bookings/", response_model=list[BookingResponseSchema])
+# async def get_bookings(
+#     current_admin: User = Security(get_current_admin),
+#     booking_service: BookingService = Depends(get_booking_service),
+#     filters: Optional[BookingResponseSchema] = Query(...),
+#     skip: int = 0,
+#     limit: int = 10,
+# ) -> list[BookingResponseSchema]:
+#     pass
+
+
+@router.get("/payments/", response_model=list[PaymentResponseSchema])
+async def list_payments(
+    skip: int = 0,
+    limit: int = 10,
+    current_admin: User = Security(get_current_admin),
+    payment_service: PaymentsService = Depends(get_payment_service),
+) -> list[PaymentResponseSchema]:
+    return await payment_service.get_all_payments(skip=skip, limit=limit)
 
 
 @router.patch("/bookings/{booking_id}", response_model=BookingResponseSchema)
